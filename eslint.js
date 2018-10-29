f9b52303cebf7691e270bb43d9b1cef6369a2333
Dipti Nayan <diptinayan21@gmail.com>
2018-10-30 02:00:34 +0530

    Create eslint.js

---
+++ eslint.js
@@ -0,0 +1,28 @@
+{
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es6": true,
+        "node": true
+    },
+    "parserOptions": {
+        "ecmaVersion": 2017,
+        "sourceType": "module",
+        "ecmaFeatures": {
+            "jsx": true
+        }
+    },
+    "rules": {
+        "no-const-assign": "warn",
+        "no-this-before-super": "warn",
+        "no-undef": "warn",
+        "no-unreachable": "warn",
+        "no-unused-vars": "warn",
+        "no-console":  "warn",
+        "no-useless-escape": "warn",
+        "constructor-super": "warn",
+        "valid-typeof": "warn",
+        "indent": ["error", 2]
+    },
+    "extends": "eslint:recommended"
+}
