36ec739b98d9a6f6ec8ad93c34988d7d49178026
dipti <diptinayan21@gmail.com>
2018-10-30 21:31:54 +0530

    files added

---
+++ js/dbhelper.js
@@ -0,0 +1,217 @@
+import * as idb from 'idb';
+
+
+class DBHelper {
+
+  /**
+   * Class constructor, opens and stored IDB referense
+   */
+  constructor() {
+    this.dbPromise = DBHelper.openDatabase();
+  }
+
+
+  /**
+   * Data API URL
+   */
+  static get RESTAURANTS_API_URL() {
+    const port = 1337; // data server port
+    return `http://localhost:${port}/restaurants`;
+  }
+
+  /**
+   * Database URL.
+   * Change this to restaurants.json file location on your server.
+   */
+  static get DATABASE_URL() {
+    const port = 1337; // Change this to your server port
+    return `http://localhost:${port}/restaurants`;
+  }
+
+
+  static openDatabase() {
+    // No service worker support =>
+    // we don't care about having a database
+    if (!navigator.serviceWorker) {
+      return Promise.resolve();
+    }
+
+    return idb.open('restaurants-db', 1, function (upgradeDb) {
+      var store = upgradeDb.createObjectStore('restaurants', { keyPath: 'id' });
+      store.createIndex('by-cusine', 'cuisine_type');
+      store.createIndex('by-neighborhood', 'neighborhood');
+    });
+  }
+
+  static fetchRestaurantsFromNetwork() {
+    return fetch(DBHelper.RESTAURANTS_API_URL)
+      .then(response => response.json());
+  }
+
+  
+  initData() {
+    if (!this.dbPromise) return;
+
+    return this.dbPromise.then(async function (db) {
+      if (!db) return;
+
+      const tx1 = db.transaction('restaurants', 'readwrite');
+      const store1 = tx1.objectStore('restaurants');
+      const count = await store1.count();
+
+      let transactionPromiseResult = tx1.complete;
+      if (count == 0) {
+        const restaurants = await DBHelper.fetchRestaurantsFromNetwork();
+        if (restaurants) {
+          const tx2 = db.transaction('restaurants', 'readwrite');
+          const store2 = tx2.objectStore('restaurants');
+          restaurants.forEach((resto) => store2.put(resto));
+          transactionPromiseResult = tx2.complete;
+        }
+      }
+
+      return transactionPromiseResult;
+
+    });
+  }
+
+  
+  fetchRestaurants() {
+    if (!this.dbPromise) return [];
+
+    return this.dbPromise.then(function (db) {
+      if (!db) return [];
+
+      const tx = db.transaction('restaurants');
+      const store = tx.objectStore('restaurants');
+
+      return store.getAll();
+    });
+  }
+
+  fetchRestaurantById(id) {
+    if (!this.dbPromise) return;
+    
+    return this.dbPromise.then(function (db) {
+      if (!db) return;
+
+      const tx = db.transaction('restaurants');
+      const store = tx.objectStore('restaurants');
+
+      return store.get(parseInt(id));
+    });
+  }
+
+ 
+  fetchRestaurantByCuisine(cuisine) {
+    if (!this.dbPromise) return;
+    return this.dbPromise.then(function (db) {
+      if (!db)  return;
+      const tx = db.transaction('restaurants');
+      const store = tx.objectStore('restaurants');
+      const cusineIdx = store.index('by-cusine');
+      return cusineIdx.getAll(cuisine);     
+    });
+  }
+
+ 
+  fetchRestaurantByNeighborhood(neighborhood) {
+    if (!this.dbPromise) return;
+    return this.dbPromise.then(function (db) {
+      if (!db)  return;
+      const tx = db.transaction('restaurants');
+      const store = tx.objectStore('restaurants');
+      const cusineIdx = store.index('by-neighborhood');
+      return cusineIdx.getAll(neighborhood);     
+    });
+  }
+
+  
+  async fetchRestaurantByCuisineAndNeighborhood(cuisine, neighborhood) {
+    if (cuisine != 'all') { // filter by cuisine
+      return this.fetchRestaurantByCuisine(cuisine)
+        .then(restaurantsByCusine => {
+          if (neighborhood != 'all') { // filter by neighborhood
+            restaurantsByCusine = restaurantsByCusine.filter(r => r.neighborhood == neighborhood);
+          }
+          return restaurantsByCusine;
+        });
+    }
+    if (neighborhood != 'all') { // filter by neighborhood
+      return this.fetchRestaurantByNeighborhood(neighborhood);
+    }
+    return this.fetchRestaurants();
+  }
+
+  
+  fetchNeighborhoods() {
+    return this.getUniqueValuesFromIndex('by-neighborhood', 'neighborhood');
+  }
+
+  
+  fetchCuisines() {
+    return this.getUniqueValuesFromIndex('by-cusine', 'cuisine_type');
+  }
+
+
+  getUniqueValuesFromIndex(indexName, indexedFieldName) {
+    if (!this.dbPromise) return;
+    return this.dbPromise.then(async function (db) {
+      if (!db)
+        return;
+      const uniqueValues = [];
+      const tx = db.transaction('restaurants');
+      const store = tx.objectStore('restaurants');
+      const cusineIdx = store.index(indexName);
+      let nextValue = null;
+      return cusineIdx.openCursor().then(function cursorIterate(cursor) {
+        if (!cursor)
+          return uniqueValues;
+        const value = cursor.value[indexedFieldName];
+        if (nextValue != value) {
+          uniqueValues.push(cursor.value[indexedFieldName]);
+          nextValue = cursor.value[indexedFieldName];
+        }
+        return cursor.continue().then(cursorIterate);
+      });
+    });
+  }
+
+  /**
+   * Restaurant page URL.
+   */
+  static urlForRestaurant(restaurant) {
+    return (`./restaurant.html?id=${restaurant.id}`);
+  }
+
+  /**
+   * Restaurant image URL.
+   */
+  static imageUrlForRestaurant(restaurant) {
+    if (!restaurant.photograph) {
+      return undefined;
+    }
+    return (`/photos/${restaurant.photograph}`);
+  }
+
+
+
+  /**
+   * Map marker for a restaurant.
+   */
+  static mapMarkerForRestaurant(restaurant, map) {
+    if (!google) return;
+    const marker = new google.maps.Marker({
+      position: restaurant.latlng,
+      title: restaurant.name,
+      url: DBHelper.urlForRestaurant(restaurant),
+      map: map,
+      animation: google.maps.Animation.DROP
+    }
+    );
+    return marker;
+  }
+
+}
+
+export default DBHelper;
